ea2d3b92ba196404766a6ad2d867b96ae4000981
Jordan <harris.jordant@gmail.com>
2015-02-09 17:19:13 -0500

    Got the parse function working correctly

--- parse.cpp
+++ parse.cpp
@@ -2,27 +2,37 @@
 using namespace std;
 
 void parse(char* cmd, char** tokens) {
+	int i = 0;
+	int j = 0;
+	int k = 0;
 
-	char** arrayOfTokens[80];
+	while(cmd[i] != '\0') {
+		if(cmd[i] == ' ') {
+			cmd[i] = '\0';
+			tokens[k] = &cmd[j];
+			j = i+1;
+			k++;
+		}
+		i++;
+	}
 
-	tokens = &cmd;
+	tokens[k] = &cmd[j];
 
-	arrayOfTokens[0] = tokens;
-	cout << *arrayOfTokens[0];
-
+	for(int index = 0; index <= k; index++) {
+		cout << tokens[index] << endl;
+	}
 }
 
 int main() {
 
-	char userInput[80];
-	char* inputPointer;
-	char** tokens;
+	char input[80];
+	cout << "Enter command: ";
+	cin.getline(input, 80);
 
-	cout << "Please enter a command: ";
-	cin.getline(userInput, 80);
-	inputPointer = userInput;
+	char* cmd = input;
+	char** tokens = &cmd;
 
-	parse(inputPointer, tokens);
+	parse(cmd, tokens);
 
 	return 0;
 }

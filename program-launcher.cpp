76e3593e1f905c5b163ee53cbfa9a749c3ad2209
Jordan <harris.jordant@gmail.com>
2015-02-12 12:44:06 -0500

    Final commit

--- program-launcher.cpp
+++ program-launcher.cpp
@@ -87,7 +87,7 @@ int main() {
 	cout << "-------------------------------------------------" << endl;
 	cout << "Enter command: ";
 
-	//Gets input from the use
+	//Gets input from the user
 	cin.getline(cmd, 100);
 
 	// Parses the user's input
